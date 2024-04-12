5f89650cb9d0f17f60af735d0d7430463ac44479
DingWoonee <slm0523@naver.com>
2024-04-12 14:39:20 +0900

    김성운 fileManager

--- src/file/FileManager.java
+++ src/file/FileManager.java
@@ -2,6 +2,9 @@ package file;
 
 import entity.*;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,11 +16,16 @@ public class FileManager {
     public static List<Ticket> ticketInfoList = new ArrayList<>();
     public static Manager manager = new Manager("1234");
 
+    private final static String movieListFileName = "movie_list.txt";
+    private final static String ticketInfoFileName = "ticket_info.txt";
+
     public static void inputDate() {
 
     }
 
-    public static void saveMovie() {
+    // 영화 리스트 저장.
+    // 저장 성공 시 true, 실패 시 false 반환.
+    public static boolean saveMovie() {
         String fileContent = "";
         for (Movie movie : movieList) {
             String newLine = movie.getName() + "$" + movie.getInfo() + "$";
@@ -29,10 +37,44 @@ public class FileManager {
                     newLine += (theater + "|");
                 }
             }
+            newLine += "$" + movie.getTime() + "\n";
+            fileContent += newLine;
+        }
+        // fileContent 변수의 내용을 movie_list.txt에 덮어씀
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(movieListFileName))) {
+            bw.write(fileContent);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
-    public static void saveTicketInfo() {
+    public static boolean saveTicketInfo() {
+        String fileContent = "";
+        for (Ticket ticket : ticketInfoList) {
+            String newLine = ticket.getReservationId() + "$" + ticket.getReservationPw() + "$" + ticket.getDetailId() + "$" + ticket.getSeatCode() + "\n";
 
+            fileContent += newLine;
+        }
+        // fileContent 변수의 내용을 movie_list.txt에 덮어씀
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(ticketInfoFileName))) {
+            bw.write(fileContent);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            List<String> a = new ArrayList<>();
+            a.add("01");
+            a.add("02");
+            Movie movie = new Movie("이름", "재밌어요", a, MovieTime.Time1);
+            movieList.add(movie);
+        }
+        //saveMovie();
     }
 }
