c83356e87c5dd646786a13f1c89581f9eeb62b55
imjaehyeong <dlawogud517@gmail.com>
2024-04-12 10:26:36 +0900

    reversation 진행중

--- src/reservation/Reservation.java
+++ src/reservation/Reservation.java
@@ -2,6 +2,8 @@ package reservation;
 
 import file.FileManager;
 
+import java.util.Scanner;
+
 public class Reservation {
     private FileManager fileManager;
 
@@ -9,17 +11,23 @@ public class Reservation {
         this.fileManager = fileManager;
     }
     public void run() {
-        movieChoice();
-        movieInfo();
+        int inputNumber = movieChoice();
+        movieInfo(inputNumber);
         peopleCount();
         seatChoice();
         password();
         reservationInfo();
     }
-    public void movieChoice(){
+    public int movieChoice(){
+        System.out.println(FileManager.movieList);
+        System.out.println("[영화선택]");
+        System.out.println("번호입력(숫자만입력):");
+        Scanner scanner = new Scanner(System.in);
+        int inputNumber = scanner.nextInt();
+        return inputNumber;
 
     }
-    public void movieInfo(){
+    public void movieInfo(int number){
 
     }
     public void peopleCount(){
