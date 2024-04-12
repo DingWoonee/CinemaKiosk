defad4957229634af555867b34141a4bd3ae77d4
DingWoonee <slm0523@naver.com>
2024-04-12 11:15:49 +0900

    file check edit

--- src/file/FileCheck.java
+++ src/file/FileCheck.java
@@ -24,17 +24,23 @@ public class FileCheck {
 
     private boolean checkMovieList() {
         try (BufferedReader br = new BufferedReader(new FileReader(movieListFileName))) {
+            // 여기서 파일 내용 한줄 한줄 검사하고 FileManager.movieList에 데이터 하나씩 추가.
+            // 한줄 한줄 검사하는데, 형식에 안 맞는게 한 줄이라도 있으면 바로 return false하면됨.
+            // 아래는 일단 테스트로 파일 내용 출력하는 코드임.
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line); // 파일 내용을 한 줄씩 읽어서 출력
             }
         } catch (FileNotFoundException e) {
+            // 파일이 없을 때 파일 생성하는 부분.
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(movieListFileName))) {
             } catch (IOException e2) {
                 e2.printStackTrace();
+                return false;
             }
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
         return true;
     }
@@ -53,5 +59,11 @@ public class FileCheck {
 
     private boolean checkManagerInfo() {
         return true;
+    }
+
+    public static void main(String[] args) {
+        // 테스트를 위한 임시 테스트 main함수
+        FileCheck fileCheck = new FileCheck();
+        fileCheck.checkMovieList();
     }
 }
