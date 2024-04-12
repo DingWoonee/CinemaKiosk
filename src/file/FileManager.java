fa2d7323e9716f267369cf72c8340210225c9ed0
DingWoonee <slm0523@naver.com>
2024-04-12 13:00:33 +0900

    김성운

--- src/file/FileManager.java
+++ src/file/FileManager.java
@@ -6,11 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
+    public static String todayDate = "";
     public static List<Movie> movieList = new ArrayList<>();
     public static List<Seat> seatList = new ArrayList<>();
     public static List<MovieDetail> movieDetailList = new ArrayList<>();
     public static List<Ticket> ticketInfoList = new ArrayList<>();
     public static Manager manager = new Manager("1234");
+
+    public static void inputDate() {
+
+    }
 
     public static void saveMovie() {
         String fileContent = "";
